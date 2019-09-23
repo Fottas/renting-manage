1362d3f3192b941c1f3ef4af087fa05ab6ce617c
yhau <onetoyin@163.com>
2019-09-23 22:11:03 +0800

    新增房源服务

---
+++ renting-manage-dubbo-server/renting-manage-dubbo-server-house-resources/renting-manage-dubbo-server-house-resources-dubbo-interface/src/main/java/com/yhau/renting/dubbo/server/api/ApiHouseResourcesService.java
@@ -0,0 +1,11 @@
+package com.yhau.renting.dubbo.server.api;
+
+import com.yhau.renting.dubbo.server.pojo.HouseResources;
+
+public interface ApiHouseResourcesService {
+    /**
+     * 新增房源
+     * @param houseResources
+     * @return -1:输入的参数不符合要求，0：数据插入数据库失败，1：成功
+     */
+    int saveHouseResources(HouseResources houseResources); }
